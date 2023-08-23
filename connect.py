0f2b023871a29779f4222c3aea083a32d7bdddfd
Kehinde Abioye <kabioye2@gmail.com>
2023-08-22 23:18:17 -0500

    Update connect.py to original

--- connect.py
+++ connect.py
@@ -1,12 +1,10 @@
 import simplepyble
-import struct
 
 if __name__ == "__main__":
     adapters = simplepyble.Adapter.get_adapters()
 
     if len(adapters) == 0:
         print("No adapters found")
-        exit()
 
     # Query the user to pick an adapter
     print("Please select an adapter:")
@@ -26,10 +24,6 @@ if __name__ == "__main__":
     adapter.scan_for(5000)
     peripherals = adapter.scan_get_results()
 
-    if len(peripherals) == 0:
-        print("No peripherals found")
-        exit()
-
     # Query the user to pick a peripheral
     print("Please select a peripheral:")
     for i, peripheral in enumerate(peripherals):
@@ -47,24 +41,5 @@ if __name__ == "__main__":
         print(f"Service: {service.uuid()}")
         for characteristic in service.characteristics():
             print(f"    Characteristic: {characteristic.uuid()}")
-
-            # Assuming the data characteristic UUID is 979b5a11-81e8-4531-89c9-90024f2984a2
-            if characteristic.uuid() == "979b5a11-81e8-4531-89c9-90024f2984a2":
-                print("Subscribing to notifications...")
-                characteristic.start_notify()
-
-                # Handle notifications from the data characteristic
-                while True:
-                    try:
-                        notification = characteristic.wait_for_notification(timeout=10)
-                        if notification:
-                            light_data = notification.data
-                            light_value = struct.unpack('<f', light_data)[0]
-                            print(f"Received Light Sensor Data: {light_value}")
-                    except simplepyble.exceptions.BLENotificationTimeout:
-                        print("No notification received within the timeout.")
-                        break
-
-                characteristic.stop_notify()
 
     peripheral.disconnect()
